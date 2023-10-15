ae9f292b24a19c1126ba15e9e2a7ce1fb0e5ebff
Swadhin Ghosh <43653792+Swadhincse@users.noreply.github.com>
2023-10-15 23:42:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,36 @@
-// complete this js code
-function Person(name, age) {}
+// Constructor function for Person
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
+}
 
-function Employee(name, age, jobTitle) {}
+// Add a greet method to the Person prototype
+Person.prototype.greet = function () {
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+};
 
-// Do not change code below this line
-window.Person = Person;
-window.Employee = Employee;
+// Constructor function for Employee
+function Employee(name, age, jobTitle) {
+    // Call the Person constructor to set name and age
+    Person.call(this, name, age);
+    this.jobTitle = jobTitle;
+}
+
+// Inherit the Person prototype in the Employee prototype
+Employee.prototype = Object.create(Person.prototype);
+
+// Set the constructor property back to Employee
+Employee.prototype.constructor = Employee;
+
+// Add a jobGreet method to the Employee prototype
+Employee.prototype.jobGreet = function () {
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+};
+
+// Example usage
+const person1 = new Person("Alice", 25);
+person1.greet();
+
+const employee1 = new Employee("Bob", 30, "Engineer");
+employee1.greet();
+employee1.jobGreet();
